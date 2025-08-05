fb13deddab23e2fa3772911f9b7e0bd80130e8fd
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-08-05 19:48:21 -0300

    Create 05_08_2025.cpp

---
+++ 05_08_2025.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <locale.h>
+using namespacestd;
+#include <locale.h>
+
+void nomeFuncao(){
+cout<<" \n Teste de acentuação...:é í ó á à â ô";
+setlocale(LC_ALL , "Portuguese");
+cout<<" \n Teste de acentuação...:é í ó á à â ô";
+}
+
+int main ()
+nomeFuncao();
+return 0;
+}
